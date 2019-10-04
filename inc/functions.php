3e42731926f57f2e23411568f4f62213ed26258e
Juan Lopez <juanl0215@gmail.com>
2019-10-04 12:34:45 -0400

    Added PrintQuote function to return the concatenated string

--- inc/functions.php
+++ inc/functions.php
@@ -1,12 +1,102 @@
+<?php 
+
 // PHP - Random Quote Generator
 
 // Create the Multidimensional array of quote elements and name it quotes
+
+
 // Each inner array element should be an associative array
+$quotes[] = [
+  'quote'=> 'Talk is cheap. Show me the code.',
+  'source'=> 'Linus Torvalds',
+  'citation'=> 'Twitter',
+  'year'=> '2001',
+  'tag'=> 'inspirational'
+];
+// Each inner array element should be an associative array
+$quotes[] = [
+  'quote'=> 'You miss 100% of the shots you don’t take',
+  'source'=> 'Wayne Gretzky',
+  'citation'=> '',
+  'year'=> '',
+  'tag'=> 'inspirational'
+];
+// Each inner array element should be an associative array
+$quotes[] = [
+  'quote'=> 'Debt is dumb, cash is King',
+  'source'=> 'Dave Ramsey',
+  'citation'=> '',
+  'year'=> null,
+  'tag'=> 'financial'
+];
+// Each inner array element should be an associative array
+$quotes[] = [
+  'quote'=> 'Life is 10% what happens to me and 90% of how I react to it',
+  'source'=> 'Charles Swindoll',
+  'citation'=> '',
+  'year'=> null,
+  'tag'=> 'inspirational'
+];
+// Each inner array element should be an associative array
+$quotes[] = [
+  'quote'=> "I'm back and done did it again",
+  'source'=> 'Joe Santos Garcia',
+  'citation'=> 'CodingPhase.com',
+  'year'=> '',
+  'tag'=> 'humor'
+];
+// Each inner array element should be an associative array
+$quotes[] = [
+  'quote'=> 'If you fail to plan, you are planning to fail!',
+  'source'=> 'Benjamin Franklin',
+  'citation'=> '',
+  'year'=> '',
+  'tag'=> 'inspirational'
+];
+// Each inner array element should be an associative array
+$quotes[] = [
+  'quote'=> 'Those who cannot remember the past are condemned to repeat it.',
+  'source'=> 'George Santayana',
+  'citation'=> '',
+  'year'=> 1905,
+  'tag'=> 'philosophy'
+];
+// Each inner array element should be an associative array
+$quotes[] = [
+  'quote'=> 'You better check yourself before you wreck yourself.',
+  'source'=> 'Ice Cube',
+  'citation'=> 'The Predator Album',
+  'year'=> '1992',
+  'tag'=> 'music'
+];
 
 
+// Get RandomQuote function that return a quote array
+function getRandomQuote($something){
+  $index = mt_rand(0,count($something) - 1);
+  return $something[$index];
+}
 
-// Create the getRandomQuuote function and name it getRandomQuote
+// PrintQuote function that calls getRandomQuote and assigns it to a variable then checks for optional keys and if present concats them to the htmlString.
+function printQuote(){
+  global $quotes;
+  $randomQuote = getRandomQuote($quotes);
+  $htmlString;
+  $htmlString.='<p class="quote">'.$randomQuote['quote'].'</p>';
+  $htmlString.='<p class="source">'.$randomQuote['source'];
 
-
-
-// Create the printQuote funtion and name it printQuote
+  if (!empty($randomQuote['citation'])){
+    $htmlString.='<span class="citation">'.$randomQuote['citation'].'</span>';  
+  }
+  if (!empty($randomQuote['year'])){
+    $htmlString.='<span class="year">'.$randomQuote['year'].'</span>';
+  }
+  if (!empty($randomQuote['tag'])){
+    $htmlString.='<sup class="citation">'.$randomQuote['tag'].'</sup>';
+  }
+  $htmlString.='</p>';
+  // Generates a random value for hue and then sets the body background to the "hsl" value
+  echo '<body style="background-color:hsl('.mt_rand(0,360).',75%,30%)">';
+  return $htmlString;
+}
+  
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+include 'inc/functions.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,14 +10,22 @@
   <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/styles.css">
+  <script>
+
+    const timer = setInterval(() => {
+      window.location.reload(1);
+    }, 7500);
+
+  </script>
 </head>
 <body>
   <div class="container">
     <div id="quote-box">
-      <p class="quote">Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.</p>
-      <p class="source">Patrick McKenzie<span class="citation">Twitter</span><span class="year">2016</span></p>
+      <?php echo printQuote();?>
     </div>
-    <button id="loadQuote" onclick="window.location.reload(true)" >Show another quote</button>
+    <button id="loadQuote" onclick="timer" >Show another quote</button>
   </div>
 </body>
 </html>
+
+
